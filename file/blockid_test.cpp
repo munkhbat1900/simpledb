da79820910fdabf024ef783bce9d1069414ed8e4
munku <>
2024-05-13 00:47:25 +0900

    add first test code

--- file/blockid_test.cpp
+++ file/blockid_test.cpp
@@ -2,9 +2,15 @@
 #include "blockid.hpp"
 
 namespace simpledb {
-    TEST(BlockIDTest, EqualsOperatorOverloadTest) {
-        BlockID blockID1 = BlockID("test", 21);
-        BlockID blockID2 = BlockID("test", 21);
-        EXPECT_TRUE(blockID1 == blockID1);
+    TEST(BlockIDTest, EqualsOperatorOverloadTestTrue) {
+        auto const blockID1 = BlockID("test", 21);
+        auto const blockID2 = BlockID("test", 21);
+        EXPECT_TRUE(blockID1 == blockID2);
+    }
+
+    TEST(BlockIDTest, EqualsOperatorOverloadTestFalse) {
+        auto const blockID1 = BlockID("test", 21);
+        auto const blockID2 = BlockID("test1", 211);
+        EXPECT_FALSE(blockID1 == blockID2);
     }
 }
